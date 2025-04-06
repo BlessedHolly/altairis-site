e6cae982e21d46ec8d16b26daf567faf6cfecf09
Holly <hollyyhere@gmail.com>
2025-04-06 18:42:03 +0300

    onrender server

--- src/store/apiAccountSlice.ts
+++ src/store/apiAccountSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiAccountSlice = createApi({
   reducerPath: "apiAccount",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5005/",
+    baseUrl: "https://altairis-server.onrender.com/",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("accessToken");
       if (token) {
