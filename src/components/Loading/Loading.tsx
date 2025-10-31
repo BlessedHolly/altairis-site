6546d15e8854f6092288af6c7d2b7885b777a177
Holly <hollyyhere@gmail.com>
2025-10-31 14:18:06 +0300

    loading message

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,6 +4,10 @@ function Loading() {
   return (
     <div className={styles["loading-container"]}>
       <p className={styles["loading"]}>Loading...</p>
+      <p className={styles["loading"]}>
+        Из-за хостинга может долго грузиться (около минуты). Пожалуйста,
+        подождите.
+      </p>
     </div>
   );
 }
